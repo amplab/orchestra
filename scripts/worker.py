acf59a26d098b2bcbd567e70f9d8869ad5c789f3
Philipp Moritz <pcmoritz@gmail.com>
2016-02-04 16:51:47 -0800

    fix worker.py

--- scripts/worker.py
+++ scripts/worker.py
@@ -2,8 +2,8 @@ import orchpy as op
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('server_port', type=str, help='the port to post requests to')
-parser.add_argument('client_port', type=str, help='the port to listen at')
+parser.add_argument('server_port', type=int, help='the port to post requests to')
+parser.add_argument('client_port', type=int, help='the port to listen at')
 parser.add_argument('subscriber_port', type=int, help='the port used to set up the connections')
 
 @op.distributed(str, int)
@@ -13,6 +13,6 @@ def setup(filename):
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    op.context.connect("tcp://10.0.171.131:" + args.server_port, "tcp://127.0.0.1:" + args.client_port, args.subscriber_port)
+    op.context.connect("10.0.171.131", args.server_port, args.subscriber_port, "127.0.0.1", args.client_port)
     op.register_current(globals().items())
     op.context.main_loop()
