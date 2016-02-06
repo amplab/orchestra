c042aa2d0f36eef5d24ba124a6183b186b90e467
Philipp Moritz <pcmoritz@gmail.com>
2016-02-06 14:02:13 -0800

    update shell

--- lib/orchpy/shell.py
+++ lib/orchpy/shell.py
@@ -2,12 +2,15 @@ import orchpy as op
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('server_port', type=str, help='the port to post requests to')
-parser.add_argument('subscriber_port', type=int, help='the port used to set up the connections')
+parser.add_argument('server_address', type=str, help='public ip address of the server')
+parser.add_argument('server_port', type=int, help='the port to post requests to')
+parser.add_argument('publish_port', type=int, help='the port for the publish channel')
+parser.add_argument('client_address', type=str, help='public ip address of this client')
+parser.add_argument('shell_port', type=int, help='the port at which the client listens')
 
 
 if __name__ == '__main__':
   args = parser.parse_args()
-  op.context.connect("tcp://127.0.0.1:" + args.server_port, "tcp://127.0.0.1:2222", args.subscriber_port)
+  op.context.connect(args.server_address, args.server_port, args.publish_port, args.client_address, args.shell_port)
   import IPython
   IPython.embed()
